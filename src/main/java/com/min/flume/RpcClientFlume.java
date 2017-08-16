d98f7b89ddd8cc76d35278cf3b86198de16a80dd
PC-20170304HXPM\Administrator <654279133@qq.com>
2017-08-16 14:33:10 +0800

    新建flume event 客户端

---
+++ src/main/java/com/min/flume/RpcClientFlume.java
@@ -0,0 +1,44 @@
+package com.min.flume;
+
+import java.nio.charset.Charset;
+
+import org.apache.flume.Event;
+import org.apache.flume.EventDeliveryException;
+import org.apache.flume.api.RpcClient;
+import org.apache.flume.api.RpcClientFactory;
+import org.apache.flume.event.EventBuilder;
+
+public class RpcClientFlume {
+	private RpcClient rpcClient;
+	@SuppressWarnings("unused")
+	private int port;
+	@SuppressWarnings("unused")
+	private String hostName;
+
+	// 创建连接
+	public RpcClientFlume(String hostName, int port) {
+		// TODO Auto-generated constructor stub
+		this.hostName = hostName;
+		this.port = port;
+		// 客户端
+		this.rpcClient = RpcClientFactory.getDefaultInstance(hostName, port);
+	}
+
+	// 发送event
+	public void sendEvent(String body) {
+		Event event = EventBuilder.withBody(body, Charset.forName("utf-8"));
+		try {
+			rpcClient.append(event);
+		} catch (EventDeliveryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		RpcClientFlume cFlume = new RpcClientFlume("centos131",44444);
+		cFlume.sendEvent("asdfgh");
+		cFlume.sendEvent("zxcvb");
+		cFlume.rpcClient.close();
+	}
+}
